e064c6357bb0ee2928f60ff861bf0d5087d550c7
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-02-21 18:43:35 +0300

    ~70 pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  const findFactors = (left) => {
+    let factors = [];
+    for (let i = 2; i <= left; i++) {
+      while (left % i === 0) {
+        factors.push(i);
+        left /= i;
+      }
+    }
+    return factors;
+  };
+  let multipliers = [];
+  let factors = findFactors(base);
+  let multiplier = factors[factors.length - 1];
+  while (multiplier <= number) {
+    multipliers.push(multiplier);
+    multiplier *= factors[factors.length - 1];
+  }
+  // factors.map((e) => {
+  //   let multiplier = e;
+  //   while (multiplier <= number) {
+  //     multipliers.push(multiplier);
+  //     multiplier *= e;
+  //   }
+  // });
+  const reducer = (sum, element) => sum + Math.floor(number / element);
+  return multipliers.reduce(reducer, 0);
+};
+// const zerosCount = getZerosCount(10, 10);
+// console.log(zerosCount);
